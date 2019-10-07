2f643abd45ff6cf4b6b102ba4f1e8734b14812aa
ribooki <rallemannd@gmail.com>
2019-10-07 14:11:35 +0200

    Ajout de la class Elevator

--- src/Elevator/Elevator.java
+++ src/Elevator/Elevator.java
@@ -3,14 +3,24 @@ package Elevator;
 public class Elevator {
     private int actualFloor;
     private int maxFloor;
+    private int state; // 0-stop, 1-ascend, -1-go down
 
     public Elevator(int maxFloor) {
         this.actualFloor = 0;
         this.maxFloor = maxFloor;
+        this.state = 0;
     }
 
     public int getActualFloor() {
         return actualFloor;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
     }
 
     public void nextFloor(){
